142f544edc24c274df2eb18fbb7fd5996c689b6b
paraparata <srizalmp@gmail.com>
2021-06-27 18:24:39 +0800

    fixed style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import styles from "./App.module.scss";
+import { MainView, SideBar } from "./components/base";
 
 const App = () => {
   return (
-    <div>
-      <span>First Fly!</span>
+    <div className={styles.root}>
+      <SideBar />
+      <MainView style={{ flex: 1 }} />
     </div>
   );
 };
