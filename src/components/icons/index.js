33a988d7b180533ea4ca14f7b6c249fc9f446720
paraparata <srizalmp@gmail.com>
2021-06-29 12:54:09 +0800

    add new icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,6 @@
 export { default as Chevron } from "./Chevron";
 export { default as Close } from "./Close";
+export { default as Eye } from "./Eye";
+export { default as EyeSlash } from "./EyeSlash";
 export { default as Menu } from "./Menu";
+export { default as Pin } from "./Pin";
